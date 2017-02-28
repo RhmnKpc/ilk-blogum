8809dc369b002516116f556767fe191c3fa6f33b
RhmnKpc <rahmankapucu@gmail.com>
2017-02-28 23:26:17 +0200

    Geliştirmeye ve Öğrenmeye Devam

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,10 @@ class Post (models.Model):
     baslik=models.CharField(max_length=200)
     yazi=models.TextField()
     yaratilis_tarihi=models.DateTimeField(default=timezone.now())
-    yayinlanma_tarihi=models.DateTimeField(blank=True,null=True)
+    yayin_tarihi=models.DateTimeField(blank=True,null=True)
 
     def yayinla(self):
-        self.yayinlanma_tarihi=timezone.now()
+        self.yayin_tarihi=timezone.now()
         self.save()
 
     def __str__(self):
